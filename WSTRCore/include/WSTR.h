43789c2630fac310dc779232a8f48c335bbb98e7
Adam H <ahughesformal@gmail.com>
2022-05-30 17:44:52 +0100

    Added standard GDI (plus) and winsock support

--- WSTRCore/include/WSTR.h
+++ WSTRCore/include/WSTR.h
@@ -1,11 +1,12 @@
 #pragma once
 
 //--Windows API--
-//Make sure to include/define additional windows functionality before this header
 //Consider WIN32_LEAN_AND_MEAN!
 //Use #define WIN_CLASSIC to regain original features that are excluded here
 
 #ifndef WIN_H
+	#define WIN_H
+
 	//Post linkage
 	#pragma comment(lib, "kernel32")
 	#pragma comment(lib, "user32")
@@ -23,6 +24,65 @@
 		#include "Windowsx.h"
 	#endif
 
-	#define WIN_H
 #endif
 //--Windows API--
+
+//--Winsock API--
+//Use #define WIN_SOCK to access
+//Note: deprecated
+#ifdef WIN_SOCK
+#ifndef WINSOCK_H
+	#define WINSOCK_H
+
+	//Post linkage
+	#pragma comment(lib, "ws2_32")
+
+	#define _WINSOCK_DEPRECATED_NO_WARNINGS //Deprecated but intent towards use has been clear
+
+	//Library includes
+	#include <stdio.h>
+	#include <winsock2.h>
+	#include <WS2tcpip.h>
+	#include <MSWSock.h>
+
+#endif
+#endif
+//--Winsock API--
+
+//--GDI(+) API--
+
+//Activate GDI with GDI+
+#ifdef GDI_PLUS
+	#define WIN_GDI
+#endif
+
+//GDI API
+//Use #define WIN_GDI to access
+#ifdef WIN_GDI
+#ifndef WIN_GDI_H
+	#define WIN_GDI_H
+
+	//Post linkage
+	#pragma comment (lib,"gdi32")
+
+	//Library includes
+
+#endif
+#endif
+
+//GDI+ API
+//Use #define GDI_PLUS to access
+#ifdef GDI_PLUS
+#ifndef GDI_PLUS_H
+	#define GDI_PLUS_H
+
+	//Post linkage
+	#pragma comment (lib,"Gdiplus")
+
+	//Library includes
+	#include <objidl.h>
+	#include <gdiplus.h>
+
+#endif
+#endif
+//--GDI(+) API--
